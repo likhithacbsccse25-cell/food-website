7942d3e38b078b7ce1a48f7ba475b6791671ad4f
likhithacbsccse25-cell <likhithacbsccse25@rvu.edu.in>
2025-10-16 19:58:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const menuItems = [
-  { name: "Paneer Tikka", price: 180, img: "C:\Users\DELL\OneDrive\Desktop\image\Paneer tikka.jps.jpg" },
-  { name: "Masala Dosa", price: 120, img: "C:\Users\DELL\OneDrive\Desktop\image\masala dosa.jps.webp" },
-  { name: "Veg Biryani", price: 150, img:"C:\Users\DELL\OneDrive\Desktop\image\Veg Biryani.jps.jpg" },
-  { name: "Butter Naan", price: 40, img: "C:\Users\DELL\OneDrive\Desktop\image\butter naan.jps.jpg"},
-  { name: "Chole Bhature", price: 130, img:"C:\Users\DELL\OneDrive\Desktop\image\Chole Bhature.jps.jpeg" },
-  { name: "Gulab Jamun", price: 60, img: "C:\Users\DELL\OneDrive\Desktop\image\Gulab Jamun.jps.webp"},
-  { name: "Mango Lassi", price: 70, img:"C:\Users\DELL\OneDrive\Desktop\image\Mango Lassi.jps.jpg"},
-  { name: "Tandoori Chicken", price: 220, img: "C:\Users\DELL\OneDrive\Desktop\image\Tandoori Chicken.jps.jpg" },
-  { name: "Pav Bhaji", price: 110, img:"C:\Users\DELL\OneDrive\Desktop\image\Pav Bhaji.jps.jpg" }
+  { name: "Paneer Tikka", price: 180, img:"https://spicecravings.com/wp-content/uploads/2020/10/Paneer-Tikka-Featured-1.jpg" },
+  { name: "Masala Dosa", price: 120, img: "https://www.mydelicious-recipes.com/home/images/120_1080_1080/mydelicious-recipes-masala-dosa-with-batter" },
+  { name: "Veg Biryani", price: 150, img:"https://www.whiskaffair.com/wp-content/uploads/2020/08/Veg-Biryani-2-3.jpg" },
+  { name: "Butter Naan", price: 40, img: "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/03/butter-naan-500x500.jpg"},
+  { name: "Chole Bhature", price: 130, img:"https://chefadora.b-cdn.net/medium_23ff54faa8628ac0b378003d51b400e4_511694cd35.jpg" },
+  { name: "Gulab Jamun", price: 60, img: "https://www.indianhealthyrecipes.com/wp-content/uploads/2021/11/gulab-jamun.webp"},
+  { name: "Mango Lassi", price: 70, img:"https://www.midwestliving.com/thmb/2n9egOlUYE0dnPxJoHYY7disquI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/KeyIngredient_MangoLassi_BP_1019_preview-0bdf9f28d35043748efaa9fd1c7b806c.jpg"},
+  { name: "Tandoori Chicken", price: 220, img: "https://www.easycookingwithmolly.com/wp-content/uploads/2023/11/air-fryer-whole-tandoori-chicken-3.jpg" },
+  { name: "Pav Bhaji", price: 110, img:"https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171428/YFL-Pav-Bhaji-3.jpg" }
 ];
 const menuContainer = document.getElementById("menu-items");
 const cartList = document.getElementById("cart-list");
